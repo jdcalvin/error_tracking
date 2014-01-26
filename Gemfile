4c4254b9a24b966494b397f2fc47b43bb8927872
jdcalvin <calvjd@gmail.com>
2014-01-26 12:32:29 -0800

    Calendar functional

--- Gemfile
+++ Gemfile
@@ -27,8 +27,6 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
-gem "watu_table_builder", :require => "table_builder"
-
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -4,7 +4,7 @@ module OrdersHelper
 		OrderType.find(params[:order_type_id])
 	end
 
-	def count_errors(num)
+def count_errors(num)
 		sum = 0
 		orders_by_day(num).each do |x|
 			if x.validations.where(approval:true).count > 0
@@ -24,4 +24,9 @@ module OrdersHelper
 			return Order.where('created_at >= ? and created_at < ?', num, num+1)
 	end
 
+	def quality(num)
+		errors = count_errors(num)
+		total = orders_by_day(num).count
+		return 100-(errors/total.to_f*100).round(2)
+	end
 end
