da55aa2dc4bc409fb7fedb67d5f7357519b36de3
jdcalvin <calvjd@gmail.com>
2014-02-15 13:17:50 -0800

    Seeds are now 5x more fancy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,60 +1,43 @@
-puts "Setting up sample database..."
-
-#Order Type
-order_type = OrderType.create(title:"fnfi errors")
-puts "Setting up template #{order_type.title}:"
-
-#Categories
-cat = ["city lien", "datedown", "documents", "general", "names", "policy", "taxes"]
-cat.each do |x|
-Category.create(name: x)
+def notice(item, b=1)
+	puts "-"*60
+	puts "Completed: #{item}"
+	unless b == 1
+	b.call
+	else
+	end
+	puts "-"*60
 end
 
-#Tasks
-city_lien = ["did not update", "incorrect amount"]
-datedown = ["did not run", "doc is incorrect", "wrong effective date",
-						"missed exceptions", "did not update new vesting"]
-documents = ["did not upload new docs", "labeled incorrectly", 
-						"wrong docs uploaded"]
-general = ["exceptions wrong", "wrong supp version"]
-
-names = ["did not update name", "listing old buyers", "name incorrect", 
-					"new buyers not added"]
-policy = ["did not update amount", "incorrect FNF sheet", "missed new loan",
-					"missed proposed insured", "wrong types/short term"]
-taxes = ["did not update", "shown incorrectly", "wrong data"]
-
-def convert_to_id(cat_name)
-	cat_name = Category.find_by(name: cat_name)
-	return cat_name.id.to_i
+total_time = Proc.new do
+	time = @end_time - @start_time
+	minutes = time / 60
+	seconds = (minutes % 1*100) * 0.6
+	mil = (seconds % 1*100).to_i
+	puts "Time: #{'%02d' %  minutes}:#{'%02d' % seconds}:#{%'%02d' % mil}"
 end
 
-puts "Creating tasks"
-city_lien.each do |x|
-	Task.new(description: x, category_id: convert_to_id("City Lien")).save
+Order Type
+@order_type = OrderType.create(title:"fnfi errors")
+notice @order_type.title
+
+#Categories and Tasks
+collection = {
+	"city_lien" => ["did not update", "incorrect amount"],
+	"datedown" => ["did not run", "doc is incorrect", "wrong effective date", "missed exceptions", "did not update new vesting"],
+	"documents" => ["did not upload new docs", "labeled incorrectly", "wrong docs uploaded"],
+	"general" => ["exceptions wrong", "wrong supp version"],
+	"names" => ["did not update name", "listing old buyers", "name incorrect", "new buyers not added"],
+	"policy" => ["did not update amount", "incorrect FNF sheet", "missed new loan", "missed proposed insured", "wrong types/short term"],
+	"taxes" => ["did not update", "shown incorrectly", "wrong data"]}
+
+collection.each_pair do |key, values|
+	cat = Category.create(name: key)
+	values.each do |t|
+		Task.create(description: t, order_type_id: @order_type.id, category_id: cat.id)
+	end
 end
 
-datedown.each do |x|
-	Task.new(description: x, category_id: convert_to_id("Datedown")).save
-end
-
-general.each do |x|
-	Task.new(description: x, category_id: convert_to_id("General")).save
-end
-
-names.each do |x|
-	Task.new(description: x, category_id: convert_to_id("Names")).save
-end
-
-policy.each do |x|
-	Task.new(description: x, category_id: convert_to_id("Policy")).save
-end
-
-taxes.each do |x|
-	Task.new(description: x, category_id: convert_to_id("Taxes")).save
-end
-puts "#{order_type.title} completed"
-
+notice "Tasks and Categories for #{@order_type.title}"
 #Randomizes order number
 def create_order_num
 	arr = "TST00".split(//)
@@ -69,7 +52,7 @@ def rolling
 end
 
 def have_error(order)
-	order.tasks.each do |task|
+	@order_type.tasks.each do |task|
 		Validation.create(
 			approval: rolling,
 			order_id: order.id,
@@ -79,7 +62,7 @@ end
 
 #Validations have no errors
 def no_error(order)
-	order.tasks.each do |task|
+	@order_type.tasks.each do |task|
 		Validation.create(
 			approval: false,
 			order_id: order.id,
@@ -99,32 +82,42 @@ def randomize_day(month)
 	end
 end
 
-puts "Creating 500 orders for January...this may take awhile"
-500.times do
-	order = Order.create(
-		order_type_id: 1,
-		order: create_order_num,
-		created_at: randomize_day(1))
+def create_orders_for month
+	puts "Creating orders for #{Date::MONTHNAMES[month]}...this may take awhile"
+	puts "..."
+	@start_time = Time.now
+	500.times do
+		order = Order.create(
+			order_type_id: 1,
+			order: create_order_num,
+			created_at: randomize_day(month))
 
-	#30% chance an error will occur
-	if rand(100)+1 > 70 
-		have_error(order)
-	else
-		no_error(order)
+		#30% chance an error will occur
+		if rand(100)+1 > 70 
+			have_error(order)
+		else
+			no_error(order)
+		end
+	end
+	@end_time = Time.now
+	notice("Orders for #{Date::MONTHNAMES[month]}", total_time)
+end
+
+#Main test database to test load
+create_orders_for 1
+create_orders_for 2
+
+#Test database for testing add/remove actions
+cats = {1 => [1,2,3], 2 => [1,2,3], 3 => [1,2,3]}
+
+test_type = OrderType.create(title: "Test Template")
+
+cats.each_pair do |key, values|
+	cat = Category.create(name: "Category #{key}")
+	values.each do |t|
+		Task.create(description: "Task #{key}-#{t}", category_id: cat.id, order_type_id: test_type.id)
 	end
 end
 
-puts "Creating 500 orders for February...this may take awhile"
-500.times do
-	order = Order.create(
-		order_type_id: 1,
-		order: create_order_num,
-		created_at: randomize_day(2))
+notice "Test Database"
 
-	#30% chance an error will occur
-	if rand(100)+1 > 70 
-		have_error(order)
-	else
-		no_error(order)
-	end
-end	
