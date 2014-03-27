645469db72d4dc8ccad4423697a8da516bdebaf8
jdcalvin <calvjd@gmail.com>
2014-03-26 23:49:47 -0700

    Search running as intended

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -3,13 +3,18 @@ require 'will_paginate/array'
 
 	def search
 		if params[:search] == "" || params[:search].nil?
-			@orders = nil
+			redirect_back_or_root
 			flash[:info] = "Please enter a search query"
 		else
 			@search = params[:search]
-			@orders = Order.reorder('order_name ASC').search(@search).page(params[:page]).per_page(20)
-			
+			@orders = Order.reorder('order_name ASC').search(@search).page(params[:page]).per_page(20)	
 		end
 	end
 
+
+	def redirect_back_or_root
+ 		redirect_to :back
+	rescue ActionController::RedirectBackError
+  	redirect_to root_path
+	end
 end
