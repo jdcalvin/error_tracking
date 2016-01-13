becf7289da3b216171b458263222d726a4c3d21a
John Calvin <calvjd@gmail.com>
2016-01-12 21:42:33 -0800

    format js

--- app/assets/javascripts/autoResize.js
+++ app/assets/javascripts/autoResize.js
@@ -1,16 +1,10 @@
 var autoResize = function() {
   var width = $('#content-panel').width();
-
   var textRescale = width / 42;
-
   var heightRescale = width * 0.16;
-
   var thResize = width * 0.025;
-
-    $('.calendar tr').children('td').css({"font-size":textRescale+"px", "height":heightRescale+"px"});
-    $('.calendar tr').children('th').css({"font-size":thResize+"px"});
-
-
-
-    $('calendar').css({"height": heightRescale+"px"});
+  
+  $('.calendar tr').children('td').css({"font-size":textRescale+"px", "height":heightRescale+"px"});
+  $('.calendar tr').children('th').css({"font-size":thResize+"px"});
+  $('calendar').css({"height": heightRescale+"px"});
 }
